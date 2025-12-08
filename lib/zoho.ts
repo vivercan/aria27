cd2ac14c3c2749ba644d4088106553275ae2b8e2
vivercan <vivercan@users.noreply.github.com>
2025-12-07 22:40:31 -0600

    System Upgrade: Aria27 Full Config + Credentials

---
+++ lib/zoho.ts
@@ -0,0 +1,11 @@
+import nodemailer from 'nodemailer'
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.zoho.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.ZOHO_USER,
+    pass: process.env.ZOHO_PASS,
+  },
+});
