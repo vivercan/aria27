2c6271998e23d95a027805ed41028b74e680b685
Juan Viveros <juan.viveros@trob.com.mx>
2026-02-08 17:03:59 -0600

    fix: remove deprecated experimental.outputFileTracing and standalone

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracing: true,
-  },
   compiler: {
     styledComponents: true,
   },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  output: "standalone",
 };
-
 module.exports = nextConfig;
