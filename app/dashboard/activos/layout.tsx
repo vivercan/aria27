e213de369404783f2e18dcee78231ba9f6054715
Juan Viveros <juan.viveros@trob.com.mx>
2026-02-10 14:35:11 -0600

    security: AccessGuard layouts en 7 modulos - proteccion por URL directa

---
+++ app/dashboard/activos/layout.tsx
@@ -0,0 +1,5 @@
+﻿"use client";
+import AccessGuard from "@/components/AccessGuard";
+export default function ActivosLayout({ children }: { children: React.ReactNode }) {
+  return <AccessGuard moduleKey="activos">{children}</AccessGuard>;
+}
---
+++ app/dashboard/talento/layout.tsx
@@ -0,0 +1,5 @@
+﻿"use client";
+import AccessGuard from "@/components/AccessGuard";
+export default function TalentoLayout({ children }: { children: React.ReactNode }) {
+  return <AccessGuard moduleKey="talento">{children}</AccessGuard>;
+}
